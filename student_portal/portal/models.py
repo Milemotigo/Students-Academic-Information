9a0cb937231a99a13b74c70ece625f16cf159f94
Gibexz <andrewetedu@gmail.com>
2023-11-19 01:12:07 +0100

    More optimizations on database structure

--- student_portal/portal/models.py
+++ student_portal/portal/models.py
@@ -1,25 +1,34 @@
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import User
 
 class School_Admin(models.Model):
-    """"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     department = models.CharField(max_length=30, unique=True)
-    # other field
 
+class Department_Admin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    department_id = models.IntegerField(unique=True)
+    # other fields...
 
 class Student(models.Model):
-    """"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    school_admin = models.ForeignKey(School_Admin, on_delete=models.CASCADE)
+    school_admin = models.ForeignKey(School_Admin, on_delete=models.CASCADE, null=True, blank=True)
+    department_admin = models.ForeignKey(Department_Admin, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=100)
     department = models.CharField(max_length=50)
     department_id = models.IntegerField(unique=True)
+    student_department_id = models.IntegerField(unique=True, null=True, blank=True)
     matric_number = models.CharField(max_length=20, unique=True)
     date_of_birth = models.DateField()
     state_of_origin = models.CharField(max_length=40)
-    # other field
+    # other fields...
 
+    # other fields...
+
+    # def save(self, *args, **kwargs):
+    #     if self.department_admin:
+    #         # Ensure department_admin can only access students with the same department_id
+    #         self.department_admin_id = self.department_id
+    #     super().save(*args, **kwargs)
